928e5cd03dd1e6c68f37b68207ece363f4b0d52a
info@dgt-international.com <info@dgt-international.com>
2026-09-05 09:46:10 -0400

    App Picker Component

--- App.js
+++ App.js
@@ -4,18 +4,30 @@ import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import ListingScreen from "./app/screens/ListingScreen";
-import { TextInput, Text } from "react-native";
+import { TextInput, Text, Switch } from "react-native";
 import { useState } from "react";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Camera", value: 3 },
+];
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [category, setCategory] = useState(categories[0]);
 
   return (
-    <GestureHandlerRootView>
-      <Screen>
-        <AppTextInput placeholder="Username" icon="email" />
-      </Screen>
-    </GestureHandlerRootView>
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        icon="apps"
+        placeholder="Category"
+      />
+      <AppTextInput icon="email" placeholder="Email" />
+    </Screen>
   );
 }
