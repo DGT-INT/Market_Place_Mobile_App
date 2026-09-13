e59db9dd43177d34042ed4baeb8330119ac2592e
info@dgt-international.com <info@dgt-international.com>
2026-09-12 20:45:34 -0400

    listingEditScreen

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
-import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import ListingScreen from "./app/screens/ListingScreen";
-import { TextInput, Text, Switch } from "react-native";
-import { useState } from "react";
-import AppTextInput from "./app/components/AppTextInput";
-import AppPicker from "./app/components/AppPicker";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  return <RegisterScreen></RegisterScreen>;
+  return <ListingEditScreen></ListingEditScreen>;
 }
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -31,9 +31,12 @@ function AppPicker({ icon, items, placeholder, onSelectItem, selectedItem }) {
               style={styles.icon}
             />
           )}
-          <AppText style={styles.text}>
-            {selectedItem ? selectedItem.label : placeholder}
-          </AppText>
+          {selectedItem ? (
+            <AppText style={styles.text}>{selectedItem.label}</AppText>
+          ) : (
+            <AppText style={styles.placeholder}>{placeholder}</AppText>
+          )}
+
           <MaterialCommunityIcons
             name="chevron-down"
             size={20}
@@ -76,6 +79,10 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   text: {
+    flex: 1,
+  },
+  placeholder: {
+    color: defaultStyles.colors.medium,
     flex: 1,
   },
 });
